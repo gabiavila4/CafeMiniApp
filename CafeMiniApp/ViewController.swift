e459471758f354b58a61794b2ff5143657caced5
Gabriela Avila <gabrielanicoleavila@icloud.com>
2023-09-19 09:48:54 -0500

    1st

--- CafeMiniApp/ViewController.swift
+++ CafeMiniApp/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelOutlet: UILabel!
+    
+    
+  
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       // labelOutlet.text = ""
+        
+        var menu: [String] = ["banana", "apple", "grape", "avocado", "peach"]
+        var prices: [Double] = [1.23, 1.32, 0.75, 4.0, 0.90]
+        var s: String = ""
+//        for bslh in menu
+//        {
+//            print(" \(bslh)")
+//            labelOutlet.text = labelOutlet.text! + " \(bslh), "
+//        }
+        var i = 0
+        labelOutlet.text = "hi"
+        while i < prices.count {
+
+        s = "\(s) \(menu[i])- $\(prices[i]) \n"
+            i = i + 1
+        }
+        labelOutlet.text = s
     }
 
+    
+    
+   
+   
+   
+    
 
 }
 
