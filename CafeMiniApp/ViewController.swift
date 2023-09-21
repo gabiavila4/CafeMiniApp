e9e75c27e1a643a6886916d977e60761f8bad824
Gabriela Avila <gabrielanicoleavila@icloud.com>
2023-09-21 09:49:05 -0500

    NEW

--- CafeMiniApp/ViewController.swift
+++ CafeMiniApp/ViewController.swift
@@ -19,17 +19,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cartTotOutlet: UILabel!
     
+    @IBOutlet weak var errorOutlet: UILabel!
+    
     
     var s2: String = "Cart- "
+    var i2 = 0
+    var menu: [String] = ["banana", "apple", "grape", "avocado", "peach"]
+    var prices: [Double] = [1.23, 1.32, 0.75, 4.0, 0.90]
+    var s: String = "Menu: \n"
+    var total: Double = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
        // labelOutlet.text = ""
         
-        var menu: [String] = ["banana", "apple", "grape", "avocado", "peach"]
-        var prices: [Double] = [1.23, 1.32, 0.75, 4.0, 0.90]
-        var s: String = "Menu: \n"
+        
         
 //        for bslh in menu
 //        {
@@ -50,20 +56,36 @@ class ViewController: UIViewController {
     
     @IBAction func ButtonAction(_ sender: UIButton) {
         
+        if (Int(AmtTFOutlet.text!)! >= 1)
+        {
         cart[ItemTFOutlet.text!] = Int(AmtTFOutlet.text!)
+            
+        }else
+        {
+            errorOutlet.text = "ERROR INVALID INPUT"
+        }
+        
         s2 = "Cart- "
         for (key, value) in cart{
             
           s2 = "\(s2) \(key) : \(value)\n"
             cartOutlet.text = "\(s2)"
          
-            for i in 0..< menu.count
-            {
-                if (ItemTFOutlet.text == menu[i])
+            
+            i2 = 0
+            while i2 < menu.count {
+                if (ItemTFOutlet.text == menu[i2])
                 {
-                    
-                }
+                total = total + (prices[i2] * Double(AmtTFOutlet.text!)!)
+                }//else
+                //{
+                   // errorOutlet.text = "ERROR ENTER VALID AMOUNT"
+                //}
+                i2 += 1
             }
+            cartTotOutlet.text = "$\(total)"
+            
+            
         }
     }
    
