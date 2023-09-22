c736f3a1e1e26779ace73809eb1eb6e4e957149e
Gabriela Avila <gabrielanicoleavila@icloud.com>
2023-09-22 09:48:23 -0500

    new 9/22

--- CafeMiniApp/ViewController.swift
+++ CafeMiniApp/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var errorOutlet: UILabel!
     
+    @IBOutlet weak var errorItemOutlet: UILabel!
+    
     
     var s2: String = "Cart- "
     var i2 = 0
@@ -28,20 +30,12 @@ class ViewController: UIViewController {
     var prices: [Double] = [1.23, 1.32, 0.75, 4.0, 0.90]
     var s: String = "Menu: \n"
     var total: Double = 0.0
+    var totBefore: Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-       // labelOutlet.text = ""
-        
-        
-        
-//        for bslh in menu
-//        {
-//            print(" \(bslh)")
-//            labelOutlet.text = labelOutlet.text! + " \(bslh), "
-//        }
+      
         var i = 0
         labelOutlet.text = "hi"
         while i < prices.count {
@@ -55,36 +49,55 @@ class ViewController: UIViewController {
     var cart: [String: Int] = [:]
     
     @IBAction func ButtonAction(_ sender: UIButton) {
-        
-        if (Int(AmtTFOutlet.text!)! >= 1)
-        {
-        cart[ItemTFOutlet.text!] = Int(AmtTFOutlet.text!)
-            
-        }else
+        if AmtTFOutlet.text == ""
         {
             errorOutlet.text = "ERROR INVALID INPUT"
-        }
+        }else
+        {
         
-        s2 = "Cart- "
-        for (key, value) in cart{
+        let amount = Int(AmtTFOutlet.text!) ?? 0
+        if (amount != 0)
+            {
             
-          s2 = "\(s2) \(key) : \(value)\n"
-            cartOutlet.text = "\(s2)"
-         
-            
+            //attempt
+            totBefore = 0
             i2 = 0
+            errorItemOutlet.text = ""
             while i2 < menu.count {
                 if (ItemTFOutlet.text == menu[i2])
                 {
-                total = total + (prices[i2] * Double(AmtTFOutlet.text!)!)
-                }//else
-                //{
-                   // errorOutlet.text = "ERROR ENTER VALID AMOUNT"
-                //}
+                    total = total + (prices[i2] * Double(AmtTFOutlet.text!)!)
+                    totBefore = total
+                }else if totBefore == 0
+                {
+                    errorItemOutlet.text = "ERROR ENTER VALID ITEM"
+                }
                 i2 += 1
             }
             cartTotOutlet.text = "$\(total)"
             
+            
+            //  if (Int(AmtTFOutlet.text!)! >= 1)
+            if errorItemOutlet.text == "" && Int(AmtTFOutlet.text!)! >= 1
+            {
+                cart[ItemTFOutlet.text!] = Int(AmtTFOutlet.text!)
+                
+            }else if (Int(AmtTFOutlet.text!)! < 1)
+            {
+                errorOutlet.text = "ERROR INVALID INPUT"
+            }
+            
+            s2 = "Cart- "
+            for (key, value) in cart{
+                
+                s2 = "\(s2) \(key) : \(value)\n"
+                cartOutlet.text = "\(s2)"
+                
+            }
+        }else
+            {
+            errorOutlet.text = "ERROR INVALID INPUT"
+        }
             
         }
     }
